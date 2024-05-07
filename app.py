b5de8927006b79e39efe4561e85e9b5f772a08c3
Dhavina Ocxa <103042614+dhavinaocxa@users.noreply.github.com>
2024-05-07 19:15:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import plotly.express as px
 st.subheader("VISUALISASI DATA")
 st.write("Dhavina Ocxa Dwiyantie (21082010136)")
 
+st.subheader("")
+st.subheader("Scatter Plot")
 # 1
 # reading the database
 data = pd.read_csv("https://raw.githubusercontent.com/dhavinaocxa/davis-2024/main/tips.csv")
@@ -16,9 +18,6 @@ data = pd.read_csv("https://raw.githubusercontent.com/dhavinaocxa/davis-2024/mai
 fig, ax = plt.subplots()
 scatter = ax.scatter(data['day'], data['tip'])
 
-# Adding Title to the Plot
-plt.title("Scatter Plot")
-
 # Setting the X and Y labels
 plt.xlabel('Day')
 plt.ylabel('Tip')
@@ -26,17 +25,18 @@ plt.ylabel('Tip')
 # showing the plot
 st.pyplot(fig)
 
+st.subheader("")
+st.subheader("Line Plot")
 # 2
 # draw lineplot
 fig, ax = plt.subplots() 
 sns.lineplot(x="sex", y="total_bill", data=data, ax=ax)
 
-# setting the title using Matplotlib
-ax.set_title('Line Plot')
-
 # showing the plot
 st.pyplot(fig)
 
+st.subheader("")
+st.subheader("Line Chart")
 # 3
 # plotting the scatter chart
 fig = px.line(data, y='tip', color='sex')
