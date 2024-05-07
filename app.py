a412c5e528a6fe18177275e34564e37eedf6352f
Dhavina Ocxa <103042614+dhavinaocxa@users.noreply.github.com>
2024-05-07 16:53:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import streamlit as st
 import plotly.express as px
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # reading the database
 data = pd.read_csv("tips.csv")
@@ -7,5 +9,8 @@ data = pd.read_csv("tips.csv")
 # plotting the scatter chart
 fig = px.scatter(data, x="day", y="tip", color='sex')
 
+# adding title to the scatter chart
+plt.title("Scatter Chart")
+
 # showing the plot
-fig.show()
+st.pyplot(fig)
