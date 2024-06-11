8fe0e78f3a86a7c6617455bae3c0b1f37e798bd8
Dhavina Ocxa <103042614+dhavinaocxa@users.noreply.github.com>
2024-06-11 11:24:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,6 @@ import io
 import base64
 import os
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-
-st.bar_chart(chart_data)
-
-
-
-# Initialize connection.
-# conn = st.connection('mysql', type='sql', username=st.secrets["DB_USER"], password=st.secrets["DB_PASS"], host=st.secrets["HOST"], database=st.secrets["DB"])
-# conn = st.connection(**st.secrets.db_credentials)
-conn = st.connection("mydb", type="sql", autocommit=True)
-
-# Perform query.
-df = conn.query('SELECT EnglishPromotionName, StartDate, EndDate, MaxQty from dimpromotion limit 10;', ttl=600)
-
-st.table(df)
-# Print results.
-# for row in df.itertuples():
-#     st.write(f"{row.EnglishPromotionName} , {row.MaxQty} ")
-
 # Function to convert text to speech and generate audio file
 def text_to_speech(text, filename="audio.mp3"):
     tts = gTTS(text=text, lang='id')  # Using Indonesian language
